1357965136bfab75a7eec1b66bed453e0d6cfe60
Rodrigo Ferraz Souza <42501669+Eteciano@users.noreply.github.com>
2019-02-21 00:10:23 -0300

    Add files via upload

---
+++ porco.bat
@@ -0,0 +1,220 @@
+@echo off
+echo msgbox"voce é um lixo, morreu com um pau na boca",vbInformation,"nem o windows te suporta mais">lll.vbs
+echo msgbox"Voce ganhou!!!!!!!!!!",vbInformation,"parabéns!!!!!!!!">ganhou.vbs
+echo.
+echo.
+echo.
+echo desa vez vai ser curto pq eu tava fazendo um fodastico mas o pc desligo p atuaizar
+pause
+cls
+echo.
+echo.
+echo.
+echo kuzão da porra
+pause
+:menu
+cls
+echo    -----------------------------------------------------------------------------------
+echo    -----------------------------------------------------------------------------------
+echo.
+echo                           *****************************************
+echo                           *                                       *
+echo                           *  Lixo criado por Rodrigo Ferraz Souza *
+echo                           *                                       *
+echo                           *              1 - jogar                *
+echo                           *              2 - dar o cu             *
+echo                           *              3 - sair                 *
+echo                           *                                       *
+echo                           *****************************************
+echo    -----------------------------------------------------------------------------------
+echo    -----------------------------------------------------------------------------------
+set /p es=oq vc quer fazer?   
+if %es% equ 1 goto jogo
+if %es% equ 2 goto xvideos
+if %es% equ 3 goto sair
+if %es% equ 4 goto 4
+if %es% equ xm22w goto xm22w
+if %es% equ tutorial goto tutorial
+if %es% equ suicidio goto suicidio
+:suicidio
+start ganhou.vbs
+start ganhou.vbs
+start ganhou.vbs
+start ganhou.vbs
+start ganhou.vbs
+start ganhou.vbs
+pause
+goto menu
+:xvideos
+start www.xvideos.com/?k=tutorial+de+como+dar+o+cu
+goto menu
+
+:4
+echo se ta me tirando parça?
+echo n te dei essa opçao
+echo lembre-se que
+echo o mundo gira
+echo e vacilao roda
+pause
+goto menu
+:jogo
+cls
+echo voce é um porco gordo e nojento como o criador desse lixo
+echo voce tem irmaos?
+choice /c SN
+IF errorlevel=2 goto nn
+IF errorlevel=1 goto ss
+:nn
+echo entao vai se foder maluco
+echo vai p quarto da sua mae
+echo brinca um pouco
+echo volta daqui a nove meses q agt conversa
+pause
+goto jogo
+:ss
+echo.
+echo.
+echo.
+echo.
+echo voce gosta de bacon?
+choice /C SN
+IF errorlevel=2 goto nao
+IF errorlevel=1 goto sim
+
+:nao
+echo entao passe a gostar seu bosta.
+pause
+goto menu
+:sim
+echo kr vc é canibal.
+pause
+cls
+echo assim que é daora huehuehe
+echo mas como isso nao é permitido em sua sociedade voce teve uma ideia
+echo voce vai esquartejar seus irmaos e come-los
+echo quer ir agora?
+choice /c sn
+if errorlevel=2 goto nvou
+if errorlevel=1 goto vou
+:nvou
+echo vai sim pq vc ta cm fome seu otario
+pause 
+goto vou
+:vou
+cls
+echo voce chegou na reuniao de familia
+echo so tem vc e seus irmaos la
+echo voce vai ate a cozinha e pega uma faca
+echo voce mata todos eles e se banha com o sangue deles
+echo voc os esquarteja e os coloca em uma caixa e leva para seu carro
+echo quer esconder as pistas?
+choice /c SN
+if errorlevel=2 (
+echo >pistas.txt) else (
+goto voltar)
+goto voltar
+:voltar
+cls
+echo voce voltou para sua casa e fez um belo ensopado
+echo voce esta vendo televisao
+echo a policia esta investigando o desparecimento dos seus irmaos
+echo a imprensa vai acompanhar em tempo real
+if exist pistas.txt (
+goto sfd ) else (
+goto ufa)
+:sfd
+echo voce nao escondeu as pistas,e agora ta com o cu na mao
+echo voce vai ter que fugir para o canada
+echo.
+pause
+echo.
+echo voce realiza que nao tem um puto
+echo voce vai ter que se prostituir
+echo quer fazer isso mesmo?
+choice /c SN
+if errorlevel=2 goto nnn
+if errorlevel=1 goto sss
+:nnn
+cls
+echo voce decidiu nao se prostituir
+echo como voce era canibal
+echo depois de um tempo voce adoeceu e morreu
+echo se fodeu
+pause
+goto sair 
+:sss
+cls
+echo porra
+echo porra
+echo voce vai msm fazer isso?
+pause
+cls
+echo voce apanhou do seu cafetao por ser feio demais
+echo ele te obrigou a fazer um boquete gratis enquanto te batia
+echo e a policia ouviu
+echo por sorte ou azar eles eram os mesmos caras que foram na casa do seu irmao e achavam vc suspeito
+echo mataram vc e seu cafetao
+echo voce morreu com um pau na boca
+pause
+start lll.vbs
+cls
+ 
+goto sair
+
+
+
+:ufa
+echo voce pelo menos foi esperto o suficiente para esconder as pistas
+echo agora da p vc ficar em casa tocando punheta
+echo voce é um lixo pior que o criador
+pause
+echo pena que vc vai morrer devido as doenças que voce pegou por ser um canibal
+pause
+goto sair
+:xm22w
+cls
+echo      d8888b.  .d8b.  db    db      d8b   db  .d88b.       d888888b d88888b db    db       .o88b. db    db 
+echo      88  `8D d8' `8b 88    88      888o  88 .8P  Y8.      `~~88~~' 88'     88    88      d8P  Y8 88    88 
+echo      88oodD' 88ooo88 88    88      88V8o 88 88    88         88    88ooooo 88    88      8P      88    88 
+echo      88~~~   88~~~88 88    88      88 V8o88 88    88         88    88~~~~~ 88    88      8b      88    88 
+echo      88      88   88 88b  d88      88  V888 `8b  d8'         88    88.     88b  d88      Y8b  d8 88b  d88 
+echo      88      YP   YP ~Y8888P'      VP   V8P  `Y88P'          YP    Y88888P ~Y8888P'       `Y88P' ~Y8888P' 
+
+pause
+goto menu
+:tutorial
+echo pau no teu cu
+echo te vira mlk
+echo ngm vai te dar manual
+echo unica coisa que eu posso te dar é uma surra de pau mole
+pause
+cls
+goto menu
+:sair
+pause
+del pistas.txt
+del lll.vbs
+del ganhou.vbs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
